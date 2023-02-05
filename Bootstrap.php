bcbb08bfc5d15e484ab27b120ca6579023eedb3a
Karakoram <shahrukhusmaani@live.com>
2023-02-05 13:51:52 +0530

    Removing false code

--- Bootstrap.php
+++ Bootstrap.php
@@ -3,7 +3,5 @@
 include("vendor/autoload.php");
 include("Server.php");
 include("Router.php");
-// $templates = new League\Plates\Engine('templates');
-//         $templates->addData(['company' => 'The Company Name'], 'layout');
-//         $templates->render('home', ['name' => 'Jonathan']);
+
 Route::Render();
